fd41d2dd3571f7bc4622f1c3d8b9e7871cf0ef89
eljabriyassine <yassinejabrio.2001@gmail.com>
2025-02-12 15:24:36 +0100

    feat: redirect user to home page in login

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useAuth } from "../context/AuthContext";
+
+export default function Layout() {
+  const { isAuthenticated } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      router.push("/");
+    }
+  }, [isAuthenticated, router]);
+}
